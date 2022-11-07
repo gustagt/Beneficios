83396a3d9c953f202ebfeb011acfbca4940aef91
Gustavo Silva <gust.aug.2ga@gmail.com>
2022-11-07 11:17:58 -0300

    att beneficiario e deficiente

--- Api/models/beneficiario.py
+++ Api/models/beneficiario.py
@@ -31,20 +31,47 @@ class beneficiario:
         return lista
 
     def add(self):
-        if self.telefone != "NULL" :
-            self.telefone = '\''+self.telefone+"\'"
 
-        if self.repLegal != "NULL" :
+        if self.repLegal != "NULL":
             self.repLegal = '\''+self.repLegal+"\'"
-        
-        teste=conn.execute(
-                'INSERT INTO {0} (cpf, nome, data_nascimento, endereco, telefone, celular, rg, data_obito, email, c_documentos, representante_legal) VALUES (\'{1}\',\'{2}\',\'{3}\',\'{4}\',{5},\'{6}\',\'{7}\',{8},\'{9}\',\'{10}\',{11})'.format(
-                    tabBeneficiarios, self.cpf, self.nome, self.dataNascimento, self.endereco, self.telefone, self.celular, self.rg, self.dataObito, self.email, self.cDocumentos, self.repLegal))
-        return teste
 
-    #def updateById(self):
+        conn.execute(
+            'INSERT INTO {0} (cpf, nome, data_nascimento, endereco, telefone, celular, rg, data_obito, email, c_documentos, representante_legal) VALUES (\'{1}\',\'{2}\',\'{3}\',\'{4}\',{5},{6},\'{7}\',{8},\'{9}\',\'{10}\',{11})'.format(
+                tabBeneficiarios, self.cpf, self.nome, self.dataNascimento, self.endereco, self.telefone, self.celular, self.rg, self.dataObito, self.email, self.cDocumentos, self.repLegal))
+
+        selecao = pd.read_sql_query(
+            "SELECT * FROM {0} where cpf={1}".format(tabBeneficiarios, self.cpf), conn)
+        return selecao
+
+    def selectByCpf(cpf):
+        selecao = pd.read_sql_query(
+            "SELECT * FROM {0} where cpf={1}".format(tabBeneficiarios, cpf), conn)
+        return selecao
+
+    def updateByCpf(self):
+
+        if self.repLegal != "NULL":
+            self.repLegal = '\''+self.repLegal+"\'"
+
+        if self.dataObito != "NULL":
+            self.dataObito = '\''+self.dataObito+"\'"
+
+        conn.execute(
+            'UPDATE {0} SET nome=\'{1}\', data_nascimento=\'{2}\', endereco=\'{3}\', telefone={4}, celular={5}, rg=\'{6}\', data_obito={7}, email=\'{8}\', c_documentos=\'{9}\', representante_legal={10} WHERE cpf={11}'.format(
+                tabBeneficiarios, self.nome, self.dataNascimento, self.endereco, self.telefone, self.celular, self.rg, self.dataObito, self.email, self.cDocumentos, self.repLegal, self.cpf))
+        selecao = pd.read_sql_query(
+            "SELECT * FROM {0} where cpf={1}".format(tabBeneficiarios, self.cpf), conn)
+        return selecao
+
+    def deleteByCpf(cpf):
+        conn.execute("DELETE FROM {0} WHERE cpf={1}".format(
+            tabBeneficiarios, cpf))
+        selecao = pd.read_sql_query(
+            "SELECT * FROM {0} where cpf={1}".format(tabBeneficiarios, cpf), conn)
+        return selecao
+
 
 date = datetime.date(2009, 5, 5)
-ben=beneficiario(15445445,'gustavito', date,'teste',9533,16533,'teste.testeds25a','caminho',repLegal='osnidlo')
-print(ben.add())
-
+ben = beneficiario(15445468455, 'gustavit', date, 'teste', 9533, 16533,
+                   'teste.testeds24454a', 'caminho', repLegal='osnildo', telefone=45531)
+print(beneficiario.deleteByCpf(15444))
---
+++ Api/models/idoso.py
@@ -0,0 +1,77 @@
+from datetime import datetime
+from typing import Any
+import db_connect
+import pandas as pd
+import datetime
+
+conn = db_connect.db_connect
+
+tabIdoso = 'idoso'
+
+class deficiente:
+    def __init__(self,  deficiencia, tipoDeficiencia, ocupacao, beneficiarioCpf, nCredencial="NULL", dataEmissao="NULL", dataValidade="NULL", segundaVia="NULL", terceiraVia="NULL", primeiroRecadastro="NULL",segundoRecadastro="NULL",terceiroRecadastro="NULL", observacoes="NULL",) -> None:
+        self.nCredencial = nCredencial
+        self.deficiencia = deficiencia
+        self.tipoDeficiencia = tipoDeficiencia
+        self.ocupacao = ocupacao
+        self.beneficiarioCpf = beneficiarioCpf
+        self.dataEmissao = dataEmissao
+        self.dataValidade = dataValidade
+        self.segundaVia = segundaVia
+        self.terceiraVia = terceiraVia
+        self.primeiroRecadastro = primeiroRecadastro
+        self.segundoRecadastro = segundoRecadastro
+        self.terceiroRecadastro = terceiroRecadastro
+        self.observacoes = observacoes
+
+    def listAll():
+        lista = pd.read_sql_query(
+            'SELECT * FROM {0} '.format(tabIdoso), conn)
+        return lista
+
+    def add(self):
+
+        if self.observacoes != "NULL":
+            self.observacoes = '\''+self.observacoes+"\'"
+
+        conn.execute(
+            'INSERT INTO {0} (n_credencial, deficiencia, tipo_deficiencia, ocupacao, data_emissao, data_validade, segunda_via, terceira_via, primeiro_recadastro, segundo_recadastro, terceiro_recadastro, observacoes, beneficiarios_cpf) VALUES ({1},\'{2}\',\'{3}\',\'{4}\',{5},{6},{7},{8},{9},{10},{11},{12},{13})'.format(
+                tabIdoso, self.nCredencial, self.deficiencia, self.tipoDeficiencia, self.ocupacao, self.dataEmissao, self.dataValidade, self.segundaVia, self.terceiraVia, self.primeiroRecadastro, self.segundoRecadastro, self.terceiroRecadastro, self.observacoes, self.beneficiarioCpf))
+
+        selecao = pd.read_sql_query(
+            "SELECT * FROM {0} WHERE beneficiarios_cpf={1}".format(tabIdoso, self.beneficiarioCpf), conn)
+        return selecao
+    
+    def selectByCpf(cpf):
+        selecao = pd.read_sql_query(
+            "SELECT * FROM {0} WHERE  beneficiarios_cpf={1}".format(tabIdoso, cpf), conn)
+        return selecao
+    
+    def selectByNCredencial(nCredencial):
+        selecao = pd.read_sql_query(
+            "SELECT * FROM {0} WHERE  n_credencial={1}".format(tabIdoso, nCredencial), conn)
+        return selecao
+
+    def updateByCpf(self):
+
+        if self.observacoes != "NULL":
+            self.observacoes = '\''+self.observacoes+"\'"
+            
+        conn.execute(
+            'UPDATE {0} SET deficiencia=\'{1}\', tipo_deficiencia=\'{2}\', ocupacao=\'{3}\', data_emissao={4}, data_validade={5}, segunda_via={6}, terceira_via={7}, primeiro_recadastro={8}, segundo_recadastro={9}, terceiro_recadastro={10}, observacoes={11} WHERE beneficiarios_cpf={12}'.format(
+                tabIdoso, self.deficiencia, self.tipoDeficiencia, self.ocupacao, self.dataEmissao, self.dataValidade, self.segundaVia, self.terceiraVia, self.primeiroRecadastro, self.segundoRecadastro, self.terceiroRecadastro, self.observacoes, self.beneficiarioCpf))
+        selecao = pd.read_sql_query(
+            "SELECT * FROM {0} where beneficiarios_cpf={1}".format(tabIdoso, self.beneficiarioCpf), conn)
+        return selecao
+
+    def deleteByCpf(cpf):
+        conn.execute("DELETE FROM {0} WHERE beneficiarios_cpf={1}".format(
+            tabIdoso, cpf))
+        selecao = pd.read_sql_query(
+            "SELECT * FROM {0} where beneficiarios_cpf={1}".format(tabIdoso, cpf), conn)
+        return selecao
+    
+    
+
+des = deficiente('fisica','irreversivel', 'motorista', 1544544)
+print(deficiente.deleteByCpf(1544544))
