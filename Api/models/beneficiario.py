7abe16ac1488aaf9834c89bfadf64e49ebaac51d
Gustavo Silva <gust.aug.2ga@gmail.com>
2022-12-13 12:55:11 -0300

    att

--- Api/models/beneficiario.py
+++ Api/models/beneficiario.py
@@ -1,3 +1,4 @@
+# from models import db_connect
 from models import db_connect
 import pandas as pd
 
@@ -7,11 +8,15 @@ tabBeneficiarios = 'beneficiarios'
 
 class Beneficiario:
     
-    def __init__(self, cpf, nome, dataNascimento, endereco, celular, rg, email,  genero, cDocumentos,telefone="NULL", dataObito="NULL", repLegal="NULL") -> None:
+    def __init__(self, cpf, nome, dataNascimento,  rua, bairro, numComplemento, cidade, cep, celular, rg, email,  genero, cDocumentos,telefone="NULL", dataObito="NULL", repLegal="NULL") -> None:
         self.cpf = cpf
         self.nome = nome
         self.dataNascimento = dataNascimento
-        self.endereco = endereco
+        self.rua = rua
+        self.bairro = bairro
+        self.numComplemento = numComplemento
+        self.cidade = cidade
+        self.cep = cep
         self.telefone = telefone
         self.celular = celular
         self.rg = rg
@@ -38,8 +43,8 @@ class Beneficiario:
             self.dataObito = '\''+self.dataObito+"\'"
 
         conn.execute(
-            'INSERT INTO {0} (cpf, nome, data_nascimento, genero, endereco, telefone, celular, rg, data_obito, email, c_documentos, representante_legal) VALUES (\'{1}\',\'{2}\',\'{3}\',\'{4}\',\'{5}\',{6},{7},\'{8}\',{9},\'{10}\',\'{11}\',{12})'.format(
-                tabBeneficiarios, self.cpf, self.nome, self.dataNascimento, self.genero, self.endereco, self.telefone, self.celular, self.rg, self.dataObito, self.email, self.cDocumentos, self.repLegal))
+            'INSERT INTO {0} (cpf, nome, data_nascimento, genero, rua, telefone, celular, rg, data_obito, email, c_documentos, representante_legal, bairro, cidade, num_complemento, cep) VALUES ({1},\'{2}\',\'{3}\',\'{4}\',\'{5}\',{6},{7},\'{8}\',{9},\'{10}\',\'{11}\',{12},\'{13}\',\'{14}\',\'{15}\',\'{16}\')'.format(
+                tabBeneficiarios, self.cpf, self.nome, self.dataNascimento, self.genero, self.rua, self.telefone, self.celular, self.rg, self.dataObito, self.email, self.cDocumentos, self.repLegal, self.bairro, self.cidade, self.numComplemento, self.cep))
 
         selecao = pd.read_sql_query(
             "SELECT * FROM {0} where cpf={1}".format(tabBeneficiarios, self.cpf), conn)
@@ -59,8 +64,8 @@ class Beneficiario:
             self.dataObito = '\''+self.dataObito+"\'"
 
         conn.execute(
-            'UPDATE {0} SET nome=\'{1}\', data_nascimento=\'{2}\', endereco=\'{3}\', genero=\'{4}\', telefone={5}, celular={6}, rg=\'{7}\', data_obito={8}, email=\'{9}\', c_documentos=\'{10}\', representante_legal={11} WHERE cpf={12}'.format(
-                tabBeneficiarios, self.nome, self.dataNascimento, self.endereco, self.genero, self.telefone, self.celular, self.rg, self.dataObito, self.email, self.cDocumentos, self.repLegal, self.cpf))
+            'UPDATE {0} SET nome=\'{1}\', data_nascimento=\'{2}\', rua=\'{3}\', genero=\'{4}\', telefone={5}, celular={6}, rg=\'{7}\', data_obito={8}, email=\'{9}\', c_documentos=\'{10}\', representante_legal={11}, bairro=\'{12}\', cidade=\'{13}\', num_complemento=\'{14}\', cep=\'{15}\' WHERE cpf={16}'.format(
+                tabBeneficiarios, self.nome, self.dataNascimento, self.rua, self.genero, self.telefone, self.celular, self.rg, self.dataObito, self.email, self.cDocumentos, self.repLegal, self.bairro, self.cidade, self.numComplemento, self.numComplemento, self.cpf))
         selecao = pd.read_sql_query(
             "SELECT * FROM {0} where cpf={1}".format(tabBeneficiarios, self.cpf), conn)
         return selecao
@@ -71,6 +76,12 @@ class Beneficiario:
         selecao = pd.read_sql_query(
             "SELECT * FROM {0} where cpf={1}".format(tabBeneficiarios, cpf), conn)
         return selecao
-
-
-
+    
+    def emailDisponivel(email):
+        selecao = pd.read_sql_query(
+            "SELECT email FROM {0} where email=\'{1}\'".format(tabBeneficiarios, email), conn)
+        if selecao.empty:
+            return True
+        
+        return False
+    
